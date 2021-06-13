7dc25362c98b54eb561ee99ef64815de8c63994b
Yana Rezantseva <yanarezantseva@yandex.ru>
2021-06-14 01:50:51 +0300

    js finaly

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 
 const popupOpenButtonElement = document.querySelector('.profile__button');
 
+
 console.log({popupOpenButtonElement, popupCloseButtonElement, popupElement});
 
 
@@ -19,4 +20,30 @@ const closePopup = function() {
 
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
+
+
+let formElement = popupElement.querySelector('.popup__container');
+let nameInput = popupElement.querySelector('.popup__name');
+let jobInput = popupElement.querySelector('.popup__job');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    console.log(document.querySelector('.popup__name').value);
+    console.log(document.querySelector('.popup__job').value);
+    
+    let nameTitle = document.querySelector('.profile__title');
+    let nameSubtitle = document.querySelector('.profile__subtitle');
+
+    nameTitle.textContent = nameInput.value;
+    nameSubtitle.textContent = jobInput.value;
+}
+
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+const saveFormButton = popupElement.querySelector(".popup__button")
+saveFormButton.addEventListener('click', closePopup);
+
+
+
 
