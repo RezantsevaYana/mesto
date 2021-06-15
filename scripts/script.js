cac2eab1f8fa844d2f944eae36b9f76472a5b4f5
Yana Rezantseva <yanarezantseva@yandex.ru>
2021-06-16 00:15:53 +0300

    second version

--- scripts/script.js
+++ scripts/script.js
@@ -4,17 +4,25 @@ const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 
 const popupOpenButtonElement = document.querySelector('.profile__button');
 
+let formElement = popupElement.querySelector('.popup__form');
 
-console.log({popupOpenButtonElement, popupCloseButtonElement, popupElement});
+let nameInput = popupElement.querySelector('.popup__input_info_name');
+
+let jobInput = popupElement.querySelector('.popup__input_info_job');
+
+let nameTitle = document.querySelector('.profile__title');
+
+let nameSubtitle = document.querySelector('.profile__subtitle');
 
 
+const openPopup = function () {
+    popupElement.classList.add('popup_opened');
 
-
-const openPopup = function() {
-    popupElement.classList.add('popup_opened')
+    nameInput.value = nameTitle.textContent;
+    jobInput.value = nameSubtitle.textContent;
 }
 
-const closePopup = function() {
+const closePopup = function () {
     popupElement.classList.remove('popup_opened')
 }
 
@@ -22,27 +30,18 @@ popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 
 
-let formElement = popupElement.querySelector('.popup__container');
-let nameInput = popupElement.querySelector('.popup__name');
-let jobInput = popupElement.querySelector('.popup__job');
-
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
     evt.preventDefault();
-    console.log(document.querySelector('.popup__name').value);
-    console.log(document.querySelector('.popup__job').value);
-    
-    let nameTitle = document.querySelector('.profile__title');
-    let nameSubtitle = document.querySelector('.profile__subtitle');
 
     nameTitle.textContent = nameInput.value;
     nameSubtitle.textContent = jobInput.value;
+
+    closePopup();
 }
 
 
 formElement.addEventListener('submit', formSubmitHandler);
 
-const saveFormButton = popupElement.querySelector(".popup__button")
-saveFormButton.addEventListener('click', closePopup);
 
 
 
